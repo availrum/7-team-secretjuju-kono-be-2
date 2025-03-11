06d0c4de838acba4dfdd1ca9f3f39fadd8a15014
jenny.kang <deco1202@naver.com>
2025-03-11 22:57:59 +0900

    chore: initial commit

---
+++ src/test/java/org/secretjuju/kono/KonoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.secretjuju.kono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
