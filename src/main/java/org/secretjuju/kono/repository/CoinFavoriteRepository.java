bbb796f7479a9d0f059ea689965d885f378a0240
keen1014 <ljh3149@gmail.com>
2025-03-24 16:55:20 +0900

    fix: 불필요한 관심목록Repository 항목 제거

--- src/main/java/org/secretjuju/kono/repository/CoinFavoriteRepository.java
+++ src/main/java/org/secretjuju/kono/repository/CoinFavoriteRepository.java
@@ -1,7 +1,6 @@
 package org.secretjuju.kono.repository;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.secretjuju.kono.entity.CoinFavorite;
 import org.secretjuju.kono.entity.CoinInfo;
@@ -17,20 +16,8 @@ public interface CoinFavoriteRepository extends JpaRepository<CoinFavorite, Inte
 	@Query("SELECT cf.coinInfo FROM CoinFavorite cf WHERE cf.user.id = :userId")
 	List<CoinInfo> findAllCoinInfosByUserId(@Param("userId") Integer userId);
 
-	// 특정 사용자의 특정 코인 관심 여부 조회
-	Optional<CoinFavorite> findByUserIdAndCoinInfoId(Integer userId, Integer coinInfoId);
-
-	// 특정 코인에 대한 모든 관심 정보 조회
-	List<CoinFavorite> findAllByCoinInfoId(Integer coinInfoId);
-
 	// 특정 사용자의 특정 코인 관심 정보 삭제
 	void deleteByUserIdAndCoinInfoId(Integer userId, Integer coinInfoId);
-
-	// 특정 사용자의 관심 코인 수 조회
-	long countByUserId(Integer userId);
-
-	// 특정 코인의 관심 등록 수 조회
-	long countByCoinInfoId(Integer coinInfoId);
 
 	// 특정 사용자의 특정 코인 관심 여부 확인
 	boolean existsByUserIdAndCoinInfoId(Integer userId, Integer coinInfoId);
