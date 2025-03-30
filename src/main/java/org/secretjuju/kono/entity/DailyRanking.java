364f414c651342a28f68f041fdd5cbba912ea685
sep.park <dytpq0978@naver.com>
2025-03-30 15:00:27 +0900

    fix: (Entity)User<->Ranking 연관관계 수정(User주체)

--- src/main/java/org/secretjuju/kono/entity/DailyRanking.java
+++ src/main/java/org/secretjuju/kono/entity/DailyRanking.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -27,8 +26,8 @@ public class DailyRanking {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false, unique = true)
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@Column(name = "current_total_assets", nullable = false, columnDefinition = "BIGINT UNSIGNED")
--- src/main/java/org/secretjuju/kono/entity/TotalRanking.java
+++ src/main/java/org/secretjuju/kono/entity/TotalRanking.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import jakarta.persistence.*;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -31,8 +30,8 @@ public class TotalRanking {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false, unique = true)
+	@OneToOne
+	@JoinColumn(name = "user_id", nullable = false)
 	private User user;
 
 	@Column(name = "current_total_assets", nullable = false, columnDefinition = "BIGINT UNSIGNED")
